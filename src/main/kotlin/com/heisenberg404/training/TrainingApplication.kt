f84a13145bb976c2319d2b27c67bac12f66a28a3
heisenberg404 <andresmhkun@gmail.com>
2019-03-29 09:44:56 -0500

    first commit

---
+++ src/main/kotlin/com/heisenberg404/training/TrainingApplication.kt
@@ -0,0 +1,11 @@
+package com.heisenberg404.training
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrainingApplication
+
+fun main(args: Array<String>) {
+    runApplication<TrainingApplication>(*args)
+}
---
+++ src/test/kotlin/com/heisenberg404/training/TrainingApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.heisenberg404.training
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TrainingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
